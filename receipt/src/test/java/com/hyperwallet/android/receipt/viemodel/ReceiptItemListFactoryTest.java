f5fbb628ff5eef74143ff73f8089253972a21f2f
Peter Olamit <polamit@hyperwallet.com>
2019-05-30 08:02:33 -0700

    HW-52584 draft test

--- receipt/src/test/java/com/hyperwallet/android/receipt/viemodel/ReceiptItemListFactoryTest.java
+++ receipt/src/test/java/com/hyperwallet/android/receipt/viemodel/ReceiptItemListFactoryTest.java
@@ -5,17 +5,26 @@ import com.hyperwallet.android.receipt.rule.HyperwalletExternalResourceManager;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
 
-//@RunWith(RobolectricTestRunner.class)
+import java.util.Locale;
+
+@RunWith(RobolectricTestRunner.class)
 public class ReceiptItemListFactoryTest {
 
-    //@Rule
-    private HyperwalletExternalResourceManager mExternalResourceManager = new HyperwalletExternalResourceManager();
+    @Rule
+    public HyperwalletExternalResourceManager mExternalResourceManager = new HyperwalletExternalResourceManager();
 
-    //@Test
+    @Test
     public void testConsolidateList_WithResult() throws JSONException {
         String json = mExternalResourceManager.getResourceContent("receipt_list_date_grouping_response.json");
         JSONObject object = new JSONObject(json);
         HyperwalletTransferMethod transferMethod = new HyperwalletTransferMethod(object);
+
+        Locale locale = Locale.getDefault();
+        locale.getDisplayName();
     }
 }
