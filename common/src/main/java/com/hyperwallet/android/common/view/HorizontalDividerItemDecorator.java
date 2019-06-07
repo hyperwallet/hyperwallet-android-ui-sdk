6ba7e593d8b0a9bc5007ceafacf67d3f0eaefbfa
Peter Olamit <polamit@hyperwallet.com>
2019-06-07 10:15:49 -0700

    ui updates

--- common/src/main/java/com/hyperwallet/android/common/view/HorizontalDividerItemDecorator.java
+++ common/src/main/java/com/hyperwallet/android/common/view/HorizontalDividerItemDecorator.java
@@ -14,7 +14,7 @@
  * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
-package com.hyperwallet.android.ui.view;
+package com.hyperwallet.android.common.view;
 
 import android.content.Context;
 import android.graphics.Canvas;
@@ -26,7 +26,7 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.hyperwallet.android.hyperwallet_ui.R;
+import com.hyperwallet.android.common.R;
 
 public class HorizontalDividerItemDecorator extends RecyclerView.ItemDecoration {
 
