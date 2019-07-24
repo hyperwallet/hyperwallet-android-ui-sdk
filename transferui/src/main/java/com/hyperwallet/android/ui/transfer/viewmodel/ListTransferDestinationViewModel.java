0f8def91ef833ea8995b90ace831faa0a464bb93
azakrevska-epam <Anna_Zakrevska@epam.com>
2019-07-24 17:44:48 +0300

    HW-54789. TransferDestination ViewModel

---
+++ transferui/src/main/java/com/hyperwallet/android/ui/transfer/viewmodel/ListTransferDestinationViewModel.java
@@ -0,0 +1,84 @@
+package com.hyperwallet.android.ui.transfer.viewmodel;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.hyperwallet.android.model.HyperwalletErrors;
+import com.hyperwallet.android.model.transfermethod.HyperwalletTransferMethod;
+import com.hyperwallet.android.ui.common.repository.Event;
+import com.hyperwallet.android.ui.transfermethod.repository.TransferMethodRepository;
+
+import java.util.List;
+
+public class ListTransferDestinationViewModel extends ViewModel {
+
+    private final MutableLiveData<List<HyperwalletTransferMethod>> mTransferDestinationList = new MutableLiveData<>();
+    private final MutableLiveData<Event<HyperwalletTransferMethod>> mTransferDestinationSection =
+            new MutableLiveData<>();
+    private final MutableLiveData<Event<HyperwalletErrors>> mTransferDestinationError = new MutableLiveData<>();
+    private TransferMethodRepository mTransferMethodRepository;
+
+    public ListTransferDestinationViewModel(
+            @NonNull final TransferMethodRepository transferMethodRepository) {
+        mTransferMethodRepository = transferMethodRepository;
+        loadTransferDestinationList();
+    }
+
+    public void selectTransferDestination(HyperwalletTransferMethod transferMethod) {
+        mTransferDestinationSection.postValue(new Event<>(transferMethod));
+    }
+
+    private void loadTransferDestinationList() {
+        mTransferMethodRepository.loadTransferMethods(new TransferMethodRepository.LoadTransferMethodListCallback() {
+            @Override
+            public void onTransferMethodListLoaded(List<HyperwalletTransferMethod> transferMethods) {
+                mTransferDestinationList.postValue(transferMethods);
+            }
+
+            @Override
+            public void onError(HyperwalletErrors errors) {
+                mTransferDestinationError.postValue(new Event<>(errors));
+            }
+        });
+    }
+
+    public MutableLiveData<List<HyperwalletTransferMethod>> getTransferDestinationList() {
+        return mTransferDestinationList;
+    }
+
+    public MutableLiveData<Event<HyperwalletTransferMethod>> getTransferDestinationSection() {
+        return mTransferDestinationSection;
+    }
+
+    /**
+     * @see ViewModel#onCleared()
+     */
+    @Override
+    protected void onCleared() {
+        super.onCleared();
+        mTransferMethodRepository = null;
+    }
+
+    public static class ListTransferDestinationViewModelFactory implements ViewModelProvider.Factory {
+
+        private final TransferMethodRepository mTransferMethodRepository;
+
+        public ListTransferDestinationViewModelFactory(
+                @NonNull final TransferMethodRepository transferMethodRepository) {
+            mTransferMethodRepository = transferMethodRepository;
+        }
+
+        @NonNull
+        @Override
+        public <T extends ViewModel> T create(@NonNull Class<T> modelClass) {
+            if (modelClass.isAssignableFrom(ListTransferDestinationViewModel.class)) {
+                return (T) new ListTransferDestinationViewModel(mTransferMethodRepository);
+            }
+            throw new IllegalArgumentException(
+                    "Expecting ViewModel class: " + ListTransferDestinationViewModel.class.getName());
+        }
+    }
+
+}
---
+++ transferui/src/test/java/com/hyperwallet/android/ui/transfer/viewmodel/ListTransferDestinationViewModelTest.java
@@ -0,0 +1,12 @@
+package com.hyperwallet.android.ui.transfer.viewmodel;
+
+
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+
+@RunWith(RobolectricTestRunner.class)
+public class ListTransferDestinationViewModelTest {
+
+
+
+}
