6f8e16a8dc45f5b05195c185cc4ece0fead1c9de
Danny Nip <dnip@paypal.com>
2019-10-28 14:25:27 -0700

    HW-57660 UI SDK support for Insight integration, shell methods. covereage

--- commonui/src/test/java/com/hyperwallet/android/ui/common/insight/HyperwalletInsightTest.java
+++ commonui/src/test/java/com/hyperwallet/android/ui/common/insight/HyperwalletInsightTest.java
@@ -1,6 +1,7 @@
 package com.hyperwallet.android.ui.common.insight;
 
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 import org.junit.Test;
@@ -12,6 +13,7 @@ public class HyperwalletInsightTest {
         HyperwalletInsight hyperwalletInsight1 = HyperwalletInsight.getInstance();
         HyperwalletInsight hyperwalletInsight2 = HyperwalletInsight.getInstance();
 
+        assertThat(hyperwalletInsight1, is(notNullValue()));
         assertThat(hyperwalletInsight1, is(hyperwalletInsight2));
     }
 }
