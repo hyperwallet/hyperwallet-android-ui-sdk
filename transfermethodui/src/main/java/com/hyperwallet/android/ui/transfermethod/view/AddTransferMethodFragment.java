2f7215faf08b3d229e79a91beed65378f0c43b00
Danny Nip <dnip@paypal.com>
2019-11-15 14:35:30 -0800

    HW-55370 change to use ErrorParamsBuilder

--- transfermethodui/src/main/java/com/hyperwallet/android/ui/transfermethod/view/AddTransferMethodFragment.java
+++ transfermethodui/src/main/java/com/hyperwallet/android/ui/transfermethod/view/AddTransferMethodFragment.java
@@ -46,7 +46,6 @@ import androidx.fragment.app.FragmentTransaction;
 import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 
 import com.hyperwallet.android.exception.HyperwalletException;
-import com.hyperwallet.android.insight.collect.ErrorInfo;
 import com.hyperwallet.android.model.HyperwalletError;
 import com.hyperwallet.android.model.graphql.HyperwalletFee;
 import com.hyperwallet.android.model.graphql.ProcessingTime;
@@ -58,6 +57,7 @@ import com.hyperwallet.android.model.transfermethod.HyperwalletTransferMethod;
 import com.hyperwallet.android.model.transfermethod.PayPalAccount;
 import com.hyperwallet.android.ui.R;
 import com.hyperwallet.android.ui.common.insight.HyperwalletInsight;
+import com.hyperwallet.android.ui.common.util.ErrorTypes;
 import com.hyperwallet.android.ui.transfermethod.HyperwalletTransferMethodLocalBroadcast;
 import com.hyperwallet.android.ui.transfermethod.repository.TransferMethodRepositoryFactory;
 import com.hyperwallet.android.ui.transfermethod.view.widget.AbstractWidget;
@@ -461,11 +461,11 @@ public class AddTransferMethodFragment extends Fragment implements WidgetEventLi
                         HyperwalletInsight.getInstance().trackError(requireContext(),
                                 HyperwalletInsight.PAGE_TRANSFER_METHOD_COLLECT_ACCOUNT,
                                 HyperwalletInsight.TRANSFER_METHOD_GROUP,
-                                new ErrorInfo.ErrorInfoBuilder()
-                                        .type(HyperwalletInsight.ERROR_TYPE_API)
+                                new HyperwalletInsight.ErrorParamsBuilder()
                                         .code(error.getCode())
                                         .message(error.getMessage())
-                                        .field(error.getFieldName())
+                                        .fieldName(error.getFieldName())
+                                        .type(ErrorTypes.getErrorType(error.getCode()))
                                         .build());
 
                         widget.showValidationError(error.getMessage());
