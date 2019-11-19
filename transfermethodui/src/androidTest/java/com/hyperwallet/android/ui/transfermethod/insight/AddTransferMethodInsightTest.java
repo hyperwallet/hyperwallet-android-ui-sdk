ce80073115d29025d58345e971b5db7ac7a566ce
vinotb <vinotb@paypal.com>
2019-11-19 16:20:36 +0530

    Automation for Add Transfer Method options impression and click events

--- transfermethodui/src/androidTest/java/com/hyperwallet/android/ui/transfermethod/insight/AddTransferMethodInsightTest.java
+++ transfermethodui/src/androidTest/java/com/hyperwallet/android/ui/transfermethod/insight/AddTransferMethodInsightTest.java
@@ -1,14 +1,23 @@
 package com.hyperwallet.android.ui.transfermethod.insight;
 
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.replaceText;
 import static androidx.test.espresso.matcher.ViewMatchers.assertThat;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
+import static org.hamcrest.Matchers.allOf;
 import static org.hamcrest.Matchers.is;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.timeout;
 import static org.mockito.Mockito.verify;
 
+import static java.net.HttpURLConnection.HTTP_BAD_REQUEST;
 import static java.net.HttpURLConnection.HTTP_OK;
+
+import static com.hyperwallet.android.ui.testutils.util.EspressoUtils.nestedScrollTo;
 
 import android.content.Context;
 import android.content.Intent;
@@ -17,6 +26,8 @@ import androidx.test.core.app.ApplicationProvider;
 import androidx.test.espresso.IdlingRegistry;
 import androidx.test.rule.ActivityTestRule;
 
+import com.hyperwallet.android.ui.R;
+import com.hyperwallet.android.ui.common.insight.HyperwalletInsight;
 import com.hyperwallet.android.ui.common.repository.EspressoIdlingResource;
 import com.hyperwallet.android.ui.common.util.PageGroups;
 import com.hyperwallet.android.ui.testutils.rule.HyperwalletExternalResourceManager;
@@ -36,9 +47,13 @@ import org.mockito.Captor;
 import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
 
+import java.io.IOException;
 import java.util.Map;
 
 public class AddTransferMethodInsightTest {
+    private static final String ACCOUNT_NUMBER = "8017110254";
+    private static final String ROUTING_NUMBER = "211179539";
+
     @ClassRule
     public static HyperwalletExternalResourceManager sResourceManager = new HyperwalletExternalResourceManager();
 
@@ -98,5 +113,102 @@ public class AddTransferMethodInsightTest {
 
     }
 
+    @Test
+    public void testAddTransferMethod_verifyInsightEventCreatedOnClickCreateTransferMethod() {
+        mActivityTestRule.launchActivity(null);
 
+        onView(withId(R.id.add_transfer_method_button)).perform(nestedScrollTo(), click());
+
+        verify(mHyperwalletInsightMockRule.getInsight(),
+                timeout(5000).times(1)).trackClick(any(Context.class),
+                eq(AddTransferMethodActivity.TAG),
+                eq(PageGroups.TRANSFER_METHOD),
+                eq(HyperwalletInsight.LINK_SELECT_TRANSFER_METHOD_CREATE),
+                mParamsCaptor.capture());
+
+        assertThat(mParamsCaptor.getValue().get("hyperwallet_ea_country"), is("US"));
+        assertThat(mParamsCaptor.getValue().get("hyperwallet_ea_currency"), is("USD"));
+        assertThat(mParamsCaptor.getValue().get("hyperwallet_ea_type"), is("BANK_ACCOUNT"));
+        assertThat(mParamsCaptor.getValue().get("hyperwallet_ea_profile_type"), is("INDIVIDUAL"));
+    }
+
+    @Test
+    public void testAddTransferMethod_verifyInsightEventCreatedOnClickCreateTransferMethodWithBusinessError() {
+        mMockWebServer.mockResponse().withHttpResponseCode(HTTP_BAD_REQUEST).withBody(sResourceManager
+                .getResourceContent("bank_account_duplicate_routing_response.json")).mock();
+
+        mActivityTestRule.launchActivity(null);
+
+        onView(withId(R.id.branchId)).perform(nestedScrollTo(), replaceText(ROUTING_NUMBER));
+        onView(withId(R.id.bankAccountId)).perform(nestedScrollTo(), replaceText(ACCOUNT_NUMBER));
+        onView(withId(R.id.bankAccountPurpose)).perform(nestedScrollTo(), click());
+        onView(allOf(withId(R.id.select_name), withText("Savings"))).perform(click());
+
+        onView(withId(R.id.add_transfer_method_button)).perform(nestedScrollTo(), click());
+
+        verify(mHyperwalletInsightMockRule.getInsight(),
+                timeout(5000).times(1)).trackClick(any(Context.class),
+                eq(AddTransferMethodActivity.TAG),
+                eq(PageGroups.TRANSFER_METHOD),
+                eq(HyperwalletInsight.LINK_SELECT_TRANSFER_METHOD_CREATE),
+                mParamsCaptor.capture());
+
+        assertThat(mParamsCaptor.getValue().get("hyperwallet_ea_country"), is("US"));
+        assertThat(mParamsCaptor.getValue().get("hyperwallet_ea_currency"), is("USD"));
+        assertThat(mParamsCaptor.getValue().get("hyperwallet_ea_type"), is("BANK_ACCOUNT"));
+        assertThat(mParamsCaptor.getValue().get("hyperwallet_ea_profile_type"), is("INDIVIDUAL"));
+    }
+
+    @Test
+    public void testAddTransferMethod_verifyInsightEventCreatedOnClickCreateTransferMethodWithNetworkError()
+            throws IOException {
+        mActivityTestRule.launchActivity(null);
+
+        onView(withId(R.id.branchId)).perform(nestedScrollTo(), replaceText(ROUTING_NUMBER));
+        onView(withId(R.id.bankAccountId)).perform(nestedScrollTo(), replaceText(ACCOUNT_NUMBER));
+        onView(withId(R.id.bankAccountPurpose)).perform(nestedScrollTo(), click());
+        onView(allOf(withId(R.id.select_name), withText("Savings"))).perform(click());
+
+        mMockWebServer.getServer().shutdown();
+
+        onView(withId(R.id.add_transfer_method_button)).perform(nestedScrollTo(), click());
+
+        verify(mHyperwalletInsightMockRule.getInsight(),
+                timeout(5000).times(1)).trackClick(any(Context.class),
+                eq(AddTransferMethodActivity.TAG),
+                eq(PageGroups.TRANSFER_METHOD),
+                eq(HyperwalletInsight.LINK_SELECT_TRANSFER_METHOD_CREATE),
+                mParamsCaptor.capture());
+
+        assertThat(mParamsCaptor.getValue().get("hyperwallet_ea_country"), is("US"));
+        assertThat(mParamsCaptor.getValue().get("hyperwallet_ea_currency"), is("USD"));
+        assertThat(mParamsCaptor.getValue().get("hyperwallet_ea_type"), is("BANK_ACCOUNT"));
+        assertThat(mParamsCaptor.getValue().get("hyperwallet_ea_profile_type"), is("INDIVIDUAL"));
+    }
+
+    @Test
+    public void testAddTransferMethod_verifyInsightEventCreatedOnAddTransferMethodSuccessful() {
+        mMockWebServer.mockResponse().withHttpResponseCode(HTTP_OK).withBody(sResourceManager
+                .getResourceContent("bank_account_response.json")).mock();
+
+        mActivityTestRule.launchActivity(null);
+
+        onView(withId(R.id.branchId)).perform(nestedScrollTo(), replaceText(ROUTING_NUMBER));
+        onView(withId(R.id.bankAccountId)).perform(nestedScrollTo(), replaceText(ACCOUNT_NUMBER));
+        onView(withId(R.id.bankAccountPurpose)).perform(nestedScrollTo(), click());
+        onView(allOf(withId(R.id.select_name), withText("Savings"))).perform(click());
+
+        onView(withId(R.id.add_transfer_method_button)).perform(nestedScrollTo(), click());
+
+        verify(mHyperwalletInsightMockRule.getInsight(),
+                timeout(5000).times(2)).trackImpression(any(Context.class),
+                eq(AddTransferMethodActivity.TAG),
+                eq(PageGroups.TRANSFER_METHOD),
+                mParamsCaptor.capture());
+
+        assertThat(mParamsCaptor.getValue().get("hyperwallet_ea_country"), is("US"));
+        assertThat(mParamsCaptor.getValue().get("hyperwallet_ea_currency"), is("USD"));
+        assertThat(mParamsCaptor.getValue().get("hyperwallet_ea_type"), is("BANK_ACCOUNT"));
+        assertThat(mParamsCaptor.getValue().get("hyperwallet_ea_profile_type"), is("INDIVIDUAL"));
+    }
 }
