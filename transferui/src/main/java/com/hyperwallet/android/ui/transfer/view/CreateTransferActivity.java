a8b98a78c19e21ad12a317d16a746bf93f895f2c
Flavio Mattos <fmattos@hyperwallet.com>
2019-12-27 00:50:08 -0800

    poc

--- transferui/src/main/java/com/hyperwallet/android/ui/transfer/view/CreateTransferActivity.java
+++ transferui/src/main/java/com/hyperwallet/android/ui/transfer/view/CreateTransferActivity.java
@@ -48,14 +48,13 @@ import com.hyperwallet.android.ui.user.repository.UserRepositoryFactory;
 /**
  * Create Transfer Activity
  */
-public class CreateTransferActivity extends AppCompatActivity implements OnNetworkErrorCallback,
-        Navigator<Event<Transfer>> {
+public class CreateTransferActivity extends AppCompatActivity implements OnNetworkErrorCallback {
 
     public static final String TAG = "transfer-funds:create-transfer";
     public static final String EXTRA_TRANSFER_SOURCE_TOKEN = "TRANSFER_SOURCE_TOKEN";
 
     private CreateTransferViewModel mCreateTransferViewModel;
-    private Helper mHelper;
+    private CreateTransferActivity.Helper mHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -91,9 +90,9 @@ public class CreateTransferActivity extends AppCompatActivity implements OnNetwo
         }
 
         mCreateTransferViewModel = ViewModelProviders.of(this, factory).get(CreateTransferViewModel.class);
-        mHelper = new Helper(this, mCreateTransferViewModel) {
+        mHelper = new CreateTransferActivity.Helper(mCreateTransferViewModel) {
             @Override
-            public void handleActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+            public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
                 if (requestCode == SCHEDULE_TRANSFER_REQUEST_CODE
                     && resultCode == Activity.RESULT_OK && data != null) {
                     CreateTransferActivity.this.setResult(Activity.RESULT_OK, data);
@@ -109,6 +108,11 @@ public class CreateTransferActivity extends AppCompatActivity implements OnNetwo
                     }
                     fragment.reApplyFieldRules();
                 }
+            }
+
+            @Override
+            public AppCompatActivity getHostActivity() {
+                return CreateTransferActivity.this;
             }
         };
 
@@ -128,7 +132,7 @@ public class CreateTransferActivity extends AppCompatActivity implements OnNetwo
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        mHelper.handleActivityResult(requestCode, resultCode, data);
+        mHelper.onActivityResult(requestCode, resultCode, data);
     }
 
     @Override
@@ -143,19 +147,18 @@ public class CreateTransferActivity extends AppCompatActivity implements OnNetwo
 
     public abstract static class Helper implements OnNetworkErrorCallback, Navigator<Event<Transfer>> {
 
-        private AppCompatActivity mAppCompatActivity;
         private CreateTransferViewModel mCreateTransferViewModel;
 
-        public Helper(@NonNull final AppCompatActivity appCompatActivity,
-                @NonNull final CreateTransferViewModel createTransferViewModel) {
-            mAppCompatActivity = appCompatActivity;
+        public Helper(@NonNull final CreateTransferViewModel createTransferViewModel) {
             mCreateTransferViewModel = createTransferViewModel;
         }
 
+        public abstract void onActivityResult(int requestCode, int resultCode, @Nullable Intent data);
+        public abstract AppCompatActivity getHostActivity();
 
         @Override
         public void retry() {
-            FragmentManager fragmentManager = mAppCompatActivity.getSupportFragmentManager();
+            FragmentManager fragmentManager = getHostActivity().getSupportFragmentManager();
             CreateTransferFragment fragment = (CreateTransferFragment)
                     fragmentManager.findFragmentById(R.id.create_transfer_fragment);
 
@@ -165,72 +168,61 @@ public class CreateTransferActivity extends AppCompatActivity implements OnNetwo
             fragment.retry();
         }
 
-
         @Override
         public void navigate(Event<Transfer> event) {
             if (!event.isContentConsumed()) {
-                Intent intent = new Intent(mAppCompatActivity, ScheduleTransferActivity.class);
+                Intent intent = new Intent(getHostActivity(), getHostActivity().getClass());
                 intent.putExtra(ScheduleTransferActivity.EXTRA_TRANSFER, event.getContent());
                 intent.putExtra(ScheduleTransferActivity.EXTRA_TRANSFER_METHOD,
                         mCreateTransferViewModel.getTransferDestination().getValue());
                 intent.putExtra(ScheduleTransferActivity.EXTRA_SHOW_FX_CHANGE_WARNING,
                         mCreateTransferViewModel.getShowFxRateChange().getValue());
-                mAppCompatActivity.startActivityForResult(intent, SCHEDULE_TRANSFER_REQUEST_CODE);
+                getHostActivity().startActivityForResult(intent, SCHEDULE_TRANSFER_REQUEST_CODE);
             }
         }
 
 
-    @Override
-    protected void onDestroy() {
-        TransferRepositoryFactory.clearInstance();
-        TransferMethodRepositoryFactory.clearInstance();
-        UserRepositoryFactory.clearInstance();
-        super.onDestroy();
-    }
-
         public void registerObservers() {
-            mCreateTransferViewModel.getLoadTransferRequiredDataErrors().observe(mAppCompatActivity,
-                    new Observer<Event<HyperwalletErrors>>() {
+            mCreateTransferViewModel.getLoadTransferRequiredDataErrors().observe(getHostActivity(),
+                    new Observer<Event<Errors>>() {
                         @Override
-                        public void onChanged(Event<HyperwalletErrors> event) {
+                        public void onChanged(Event<Errors> event) {
                             if (event != null && !event.isContentConsumed()) {
-                                ActivityUtils.showError(mAppCompatActivity, TAG, PageGroups.TRANSFER_FUNDS,
+                                ActivityUtils.showError(getHostActivity(), TAG, PageGroups.TRANSFER_FUNDS,
                                         event.getContent().getErrors());
                             }
                         }
                     });
 
-            mCreateTransferViewModel.getCreateTransferError().observe(mAppCompatActivity, new Observer<Event<HyperwalletErrors>>() {
+            mCreateTransferViewModel.getCreateTransferError().observe(getHostActivity(), new Observer<Event<Errors>>() {
                 @Override
-                public void onChanged(Event<HyperwalletErrors> event) {
+                public void onChanged(Event<Errors> event) {
                     if (event != null && !event.isContentConsumed()) {
-                        ActivityUtils.showError(mAppCompatActivity, TAG, PageGroups.TRANSFER_FUNDS,
+                        ActivityUtils.showError(getHostActivity(), TAG, PageGroups.TRANSFER_FUNDS,
                                 event.getContent().getErrors());
                     }
                 }
             });
 
-            mCreateTransferViewModel.getCreateTransfer().observe(mAppCompatActivity, new Observer<Event<Transfer>>() {
+            mCreateTransferViewModel.getCreateTransfer().observe(getHostActivity(), new Observer<Event<Transfer>>() {
                 @Override
                 public void onChanged(final Event<Transfer> transfer) {
                     navigate(transfer);
                 }
             });
 
-            mCreateTransferViewModel.getModuleUnavailableError().observe(mAppCompatActivity, new Observer<Event<HyperwalletErrors>>() {
+            mCreateTransferViewModel.getModuleUnavailableError().observe(getHostActivity(), new Observer<Event<Errors>>() {
                 @Override
-                public void onChanged(Event<HyperwalletErrors> event) {
+                public void onChanged(Event<Errors> event) {
                     if (!event.isContentConsumed()) {
-                        ActivityUtils.showError(mAppCompatActivity, TAG, PageGroups.TRANSFER_FUNDS,
+                        ActivityUtils.showError(getHostActivity(), TAG, PageGroups.TRANSFER_FUNDS,
                                 event.getContent().getErrors());
                     }
                 }
             });
         }
 
-
     }
-
 
 
 }
