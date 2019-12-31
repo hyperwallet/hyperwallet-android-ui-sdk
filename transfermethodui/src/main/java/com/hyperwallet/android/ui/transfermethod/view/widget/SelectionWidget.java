7ebbb5c01102d7fa45b8c4b348815c6f6655b788
Peter Olamit <polamit@hyperwallet.com>
2019-12-31 11:10:31 -0800

    forgot to set the value when using field data from gql

--- transfermethodui/src/main/java/com/hyperwallet/android/ui/transfermethod/view/widget/SelectionWidget.java
+++ transfermethodui/src/main/java/com/hyperwallet/android/ui/transfermethod/view/widget/SelectionWidget.java
@@ -78,7 +78,8 @@ public class SelectionWidget extends AbstractWidget implements WidgetSelectionDi
                     new ContextThemeWrapper(viewGroup.getContext(), R.style.Widget_Hyperwallet_TextInputEditText));
             mEditText.setTextColor(viewGroup.getContext().getResources().getColor(R.color.regularColorSecondary));
 
-            mEditText.setText(getKeyFromValue(TextUtils.isEmpty(mDefaultValue) ? mField.getValue() : mDefaultValue));
+            mEditText.setText(
+                    getKeyFromValue(TextUtils.isEmpty(mDefaultValue) ? mValue = mField.getValue() : mDefaultValue));
             setIdFromFieldLabel(mTextInputLayout);
             setIdFromFieldName(mEditText);
 
