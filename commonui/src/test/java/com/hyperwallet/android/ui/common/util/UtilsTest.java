745846cb94112cc03d602990548fa00d9333c61e
Rajkumar <rbalakrishnasamy@paypal.com>
2020-11-09 19:02:31 +0530

    DTSERWFOUR-23 - Update the test case currency

--- commonui/src/test/java/com/hyperwallet/android/ui/common/util/UtilsTest.java
+++ commonui/src/test/java/com/hyperwallet/android/ui/common/util/UtilsTest.java
@@ -28,12 +28,12 @@ public class UtilsTest {
             put("DKK", "DKK1,000,000.00");      // Denmark Currency
             put("EGP", "EGP1,000,000.00");      // Egypt Currency
             put("EUR", "€1,000,000.00");      // Austria Currency
-            put("HKD", "HK$1,000,000.00");      // Hong Kong Currency
+            put("HKD", "HKD1,000,000.00");      // Hong Kong Currency
             put("HUF", "HUF1,000,000.00");      // Hungary Currency
-            put("INR", "₹1,000,000.00");       // India Currency
+            put("INR", "INR1,000,000.00");       // India Currency
             put("IDR", "IDR1,000,000.00");         // Indonesia Currency
             put("JMD", "JMD1,000,000.00");      // Jamaica Currency
-            put("JPY", "¥1,000,000.00");         // Japan Currency
+            put("JPY", "JPY1,000,000.00");         // Japan Currency
             put("JOD", "JOD1,000,000.00");     // Jordan Currency
             put("KZT", "KZT1,000,000.00");      // Kazakhstan Currency
             put("KES", "KES1,000,000.00");      // Kenya Currency
@@ -41,8 +41,8 @@ public class UtilsTest {
             put("MYR", "MYR1,000,000.00");      // Malaysia Currency
             put("MXN", "MXN1,000,000.00");      // Mexico Currency
             put("MAD", "MAD1,000,000.00");      // Morocco Currency
-            put("ILS", "₪1,000,000.00");      // Israel Currency
-            put("TWD", "NT$1,000,000.00");      // Taiwan Currency
+            put("ILS", "ILS1,000,000.00");      // Israel Currency
+            put("TWD", "TWD1,000,000.00");      // Taiwan Currency
             put("TRY", "TRY1,000,000.00");      // Turkey Currency
             put("NZD", "NZ$1,000,000.00");      // New Zealand Currency
             put("NGN", "NGN1,000,000.00");      // Nigeria Currency
@@ -51,13 +51,13 @@ public class UtilsTest {
             put("PEN", "PEN1,000,000.00");      // Peru Currency
             put("PHP", "PHP1,000,000.00");      // Philippines Currency
             put("PLN", "PLN1,000,000.00");      // Poland Currency
-            put("GBP", "£1,000,000.00");      // Isle of Man
+            put("GBP", "GBP1,000,000.00");      // Isle of Man
             put("RON", "RON1,000,000.00");      // Romania Currency
             put("RUB", "RUB1,000,000.00");      // Russia Currency
             put("RSD", "RSD1,000,000.00");         // Serbia Currency
             put("SGD", "SGD1,000,000.00");      // Singapore Currency
             put("ZAR", "ZAR1,000,000.00");      // South Africa Currency
-            put("KRW", "₩1,000,000.00");         // South Korea Currency
+            put("KRW", "KRW1,000,000.00");         // South Korea Currency
             put("LKR", "LKR1,000,000.00");      // Sri Lanka Currency
             put("SEK", "SEK1,000,000.00");      // Sweden Currency
             put("CHF", "CHF1,000,000.00");      // Switzerland Currency
@@ -65,7 +65,7 @@ public class UtilsTest {
             put("TND", "TND1,000,000.00");     // Tunisia Currency
             put("AED", "AED1,000,000.00");      // United Arab Emirates Currency
             put("UGX", "UGX1,000,000.00");         // Uganda Currency
-            put("USD", "$1,000,000.00");      // United States Currency
+            put("USD", "USD1,000,000.00");      // United States Currency
             put("VND", "VND1,000,000.00");         // Vietnam Currency
         }
     };
