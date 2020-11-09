d1fecb0b026468a3158041aff075b9e2714afda8
Rajkumar <rbalakrishnasamy@paypal.com>
2020-11-09 18:24:14 +0530

    DTSERWFOUR-23 - Update the test cases and revert the fraction formatting.

--- commonui/src/main/java/com/hyperwallet/android/ui/common/util/Utils.java
+++ commonui/src/main/java/com/hyperwallet/android/ui/common/util/Utils.java
@@ -1,47 +1,12 @@
 package com.hyperwallet.android.ui.common.util;
 
 import java.text.NumberFormat;
-import java.util.ArrayList;
 import java.util.Currency;
-import java.util.List;
 
 /**
  * Common UI SDK utility class for formatting the currency.
  */
 public class Utils {
-
-    /**
-     * Currencies without Fraction
-     */
-    private static final List<String> NODECIMAL_CURRENCY = new ArrayList<String>() {{
-        add("JPY");
-        add("ALL");
-        add("COP");
-        add("SGD");
-        add("PKR");
-        add("LAK");
-        add("IDR");
-        add("RSD");
-        add("VND");
-        add("CLP");
-        add("UGX");
-        add("KRW");
-    }};
-
-    /**
-     * Currencies with three Fraction
-     */
-    private static final List<String> THREE_DECIMAL_CURRENCY = new ArrayList<String>() {{
-        add("JOD");
-        add("TND");
-    }};
-
-    /**
-     * Currencies with single Fraction
-     */
-    private static final List<String> SINGLE_DECIMAL_CURRENCY = new ArrayList<String>() {{
-        add("AMD");
-    }};
 
     /**
      * Formats the currency as per currency code.
@@ -53,14 +18,6 @@ public class Utils {
     public static String formatCurrency(String currency, String amount) {
         NumberFormat format = NumberFormat.getCurrencyInstance();
         format.setCurrency(Currency.getInstance(currency));
-
-        if (NODECIMAL_CURRENCY.contains(currency)) {
-            format.setMaximumFractionDigits(0);
-        } else if (THREE_DECIMAL_CURRENCY.contains(currency)) {
-            format.setMinimumFractionDigits(3);
-        } else if (SINGLE_DECIMAL_CURRENCY.contains(currency)) {
-            format.setMinimumFractionDigits(1);
-        }
         return format.format(Double.parseDouble(amount));
     }
 }
