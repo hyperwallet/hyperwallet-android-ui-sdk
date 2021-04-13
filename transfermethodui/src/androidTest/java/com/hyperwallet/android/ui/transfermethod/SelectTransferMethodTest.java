aaff7e33500060fe669c3a916fa20285c1b388f5
Shanavas sabeerahamed <71114945+shanavas123@users.noreply.github.com>
2021-04-13 21:25:58 +0530

    Task/dtserwfour 553 update transfer fee zero state (#260)
    * DTSERWFOUR-553 update transfer fee zero state
    
    * UIcase update for NoFee
    
    * DTSERWFOUR-553 update unit test
    
    * DTSERWFOUR-553 update localization string
    
    * DTSERWFOUR-553 fixed mixed fee
    
    * DTSERWFOUR-553 fixed mixed fee.
    
    * UIcase update for NoFee
    
    * Addressed review comments
    
    * Addressed review comments
    
    Co-authored-by: Naren <najayaraman@paypal.com>

--- transfermethodui/src/androidTest/java/com/hyperwallet/android/ui/transfermethod/SelectTransferMethodTest.java
+++ transfermethodui/src/androidTest/java/com/hyperwallet/android/ui/transfermethod/SelectTransferMethodTest.java
@@ -126,7 +126,7 @@ public class SelectTransferMethodTest {
         onView(allOf(instanceOf(TextView.class), withParent(withId(R.id.country_selection_toolbar)))).check(
                 matches(withText(R.string.mobileCountryRegion)));
         onView(withId(R.id.search_button)).check(doesNotExist());
-        onView(withId(R.id.country_selection_list)).check(new RecyclerViewCountAssertion(5));
+        onView(withId(R.id.country_selection_list)).check(new RecyclerViewCountAssertion(6));
         onView(allOf(withId(R.id.country_name), withText("Canada"))).check(matches(isDisplayed()));
         onView(allOf(withId(R.id.country_name), withText("Croatia"))).check(matches(isDisplayed()));
         onView(allOf(withId(R.id.country_name), withText("Mexico"))).check(matches(isDisplayed()));
@@ -207,10 +207,12 @@ public class SelectTransferMethodTest {
                 matches(atPosition(0, hasDescendant(withText(R.string.bank_account)))));
 
         String bankCardFee = InstrumentationRegistry.getInstrumentation().getTargetContext()
-                .getString(R.string.feeAndProcessingTimeInformation, usdDollarSymbol + "2.00", "1-2 Business days");
+                .getString(R.string.feeInformation, usdDollarSymbol + "2.00");
+        String bankCardProcessingTime = InstrumentationRegistry.getInstrumentation().getTargetContext()
+                .getString(R.string.processingTimeInformation, "1-2 Business days");
 
         onView(withId(R.id.select_transfer_method_types_list)).check(
-                matches(atPosition(0, hasDescendant(withText(bankCardFee)))));
+                matches(atPosition(0, hasDescendant(withText(bankCardFee+bankCardProcessingTime)))));
 
         onView(withId(R.id.select_transfer_method_types_list)).check(
                 matches(atPosition(1, hasDescendant(withText(R.string.bank_card_font_icon)))));
@@ -218,10 +220,13 @@ public class SelectTransferMethodTest {
                 matches(atPosition(1, hasDescendant(withText(R.string.bank_card)))));
 
         String debitCardFee = InstrumentationRegistry.getInstrumentation().getTargetContext()
-                .getString(R.string.feeAndProcessingTimeInformation, usdDollarSymbol + "1.75", "IMMEDIATE");
+                .getString(R.string.feeInformation, usdDollarSymbol + "1.75");
+
+        String debitCardProcessingTime = InstrumentationRegistry.getInstrumentation().getTargetContext()
+                .getString(R.string.processingTimeInformation, "IMMEDIATE");
 
         onView(withId(R.id.select_transfer_method_types_list)).check(
-                matches(atPosition(1, hasDescendant(withText(debitCardFee)))));
+                matches(atPosition(1, hasDescendant(withText(debitCardFee+debitCardProcessingTime)))));
 
 
         onView(withId(R.id.select_transfer_method_types_list)).check(
@@ -233,10 +238,12 @@ public class SelectTransferMethodTest {
                 matches(atPosition(2, hasDescendant(wireAccountViewMatchers))));
 
         String wireTransferFee = InstrumentationRegistry.getInstrumentation().getTargetContext()
-                .getString(R.string.feeAndProcessingTimeInformation, usdDollarSymbol + "20.00", "1-3 Business days");
+                .getString(R.string.feeInformation, usdDollarSymbol + "20.00");
+        String wireTransferProcessingTime = InstrumentationRegistry.getInstrumentation().getTargetContext()
+                .getString(R.string.processingTimeInformation, "1-3 Business days");
 
         onView(withId(R.id.select_transfer_method_types_list)).check(
-                matches(atPosition(2, hasDescendant(withText(wireTransferFee)))));
+                matches(atPosition(2, hasDescendant(withText(wireTransferFee+wireTransferProcessingTime)))));
 
         onView(withId(R.id.select_transfer_method_types_list)).check(
                 matches(atPosition(3, hasDescendant(withText(R.string.paypal_account_font_icon)))));
@@ -244,10 +251,12 @@ public class SelectTransferMethodTest {
                 matches(atPosition(3, hasDescendant(withText("PayPal")))));
 
         String paypalFee = InstrumentationRegistry.getInstrumentation().getTargetContext()
-                .getString(R.string.feeAndProcessingTimeInformation, usdDollarSymbol + "0.25", "IMMEDIATE");
+                .getString(R.string.feeInformation, usdDollarSymbol + "0.25");
+        String paypalProcessingTime = InstrumentationRegistry.getInstrumentation().getTargetContext()
+                .getString(R.string.processingTimeInformation, "IMMEDIATE");
 
         onView(withId(R.id.select_transfer_method_types_list)).check(
-                matches(atPosition(3, hasDescendant(withText(paypalFee)))));
+                matches(atPosition(3, hasDescendant(withText(paypalFee+paypalProcessingTime)))));
     }
 
     @Test
@@ -268,7 +277,7 @@ public class SelectTransferMethodTest {
         onView(withId(R.id.select_transfer_method_types_list)).check(
                 matches(atPosition(0, hasDescendant(withText(R.string.bank_account)))));
         onView(withId(R.id.select_transfer_method_types_list)).check(
-                matches(atPosition(0, hasDescendant(withText("1-2 Business days")))));
+                matches(atPosition(0, hasDescendant(withText("No fee \u2022 1-2 Business days")))));
     }
 
     @Test
@@ -318,10 +327,12 @@ public class SelectTransferMethodTest {
                 matches(atPosition(0, hasDescendant(withText(R.string.bank_account)))));
 
         String bankAccountFee = InstrumentationRegistry.getInstrumentation().getTargetContext()
-                .getString(R.string.feeAndProcessingTimeInformation, cdnDollarSymbol + "2.20", "1-2 Business days");
+                .getString(R.string.feeInformation, cdnDollarSymbol + "2.20");
+        String bankCardProcessingTime = InstrumentationRegistry.getInstrumentation().getTargetContext()
+                .getString(R.string.processingTimeInformation, "1-2 Business days");
 
         onView(withId(R.id.select_transfer_method_types_list)).check(
-                matches(atPosition(0, hasDescendant(withText(bankAccountFee)))));
+                matches(atPosition(0, hasDescendant(withText(bankAccountFee+bankCardProcessingTime)))));
 
         onView(withId(R.id.select_transfer_method_types_list)).check(
                 matches(atPosition(1, hasDescendant(withText(R.string.paypal_account_font_icon)))));
@@ -329,10 +340,12 @@ public class SelectTransferMethodTest {
                 matches(atPosition(1, hasDescendant(withText("PayPal")))));
 
         String paypalFee = InstrumentationRegistry.getInstrumentation().getTargetContext()
-                .getString(R.string.feeAndProcessingTimeInformation, cdnDollarSymbol + "0.25", "IMMEDIATE");
+                .getString(R.string.feeInformation, cdnDollarSymbol + "0.25");
+        String paypalProcessingTime = InstrumentationRegistry.getInstrumentation().getTargetContext()
+                .getString(R.string.processingTimeInformation, "IMMEDIATE");
 
         onView(withId(R.id.select_transfer_method_types_list)).check(
-                matches(atPosition(1, hasDescendant(withText(paypalFee)))));
+                matches(atPosition(1, hasDescendant(withText(paypalFee+paypalProcessingTime)))));
     }
 
     @Test
@@ -444,5 +457,96 @@ public class SelectTransferMethodTest {
 
         onView(withId(R.id.select_transfer_method_country_value)).check(matches(withText("United States")));
         onView(withId(R.id.select_transfer_method_currency_value)).check(matches(withText("USD")));
+    }
+
+    @Test
+    public void testSelectTransferMethod_verifyNoFeeLabelsDisplayed() {
+        mMockWebServer.mockResponse().withHttpResponseCode(HTTP_OK).withBody(sResourceManager
+                .getResourceContent("user_response.json")).mock();
+        mMockWebServer.mockResponse().withHttpResponseCode(HTTP_OK).withBody(sResourceManager
+                .getResourceContent("successful_tmc_keys_response.json")).mock();
+
+        mActivityTestRule.launchActivity(null);
+
+        onView(withId(R.id.toolbar)).check(matches(isDisplayed()));
+        onView(withText(R.string.mobileAddTransferMethodHeader)).check(matches(withParent(withId(R.id.toolbar))));
+
+        onView(withId(R.id.select_transfer_method_country_value)).check(matches(withText("United States")));
+        onView(withId(R.id.select_transfer_method_currency_value)).check(matches(withText("USD")));
+        onView(withId(R.id.select_transfer_method_types_list)).check(new RecyclerViewCountAssertion(4));
+
+        onView(withId(R.id.select_transfer_method_country_value)).perform(click());
+        onView(allOf(withId(R.id.country_name), withText("CHINA"))).perform(click());
+
+        String bankAccountFee = InstrumentationRegistry.getInstrumentation().getTargetContext()
+                .getString(R.string.noFee);
+        String bankAccountProcessingTime = InstrumentationRegistry.getInstrumentation().getTargetContext()
+                .getString(R.string.processingTimeInformation, "1-2 Business days");
+
+        onView(withId(R.id.select_transfer_method_types_list)).check(
+                matches(atPosition(0, hasDescendant(withText(bankAccountFee+bankAccountProcessingTime)))));
+
+
+        String bankCardFee = InstrumentationRegistry.getInstrumentation().getTargetContext()
+                .getString(R.string.noFee);
+        String bankCardProcessingTime = InstrumentationRegistry.getInstrumentation().getTargetContext()
+                .getString(R.string.processingTimeInformation, "IMMEDIATE");
+
+        onView(withId(R.id.select_transfer_method_types_list)).check(
+                matches(atPosition(1, hasDescendant(withText(bankCardFee+bankCardProcessingTime)))));
+
+
+
+        String wireTransferFee = InstrumentationRegistry.getInstrumentation().getTargetContext()
+                .getString(R.string.noFee);
+        String wireTransferProcessingTime = InstrumentationRegistry.getInstrumentation().getTargetContext()
+                .getString(R.string.processingTimeInformation, "1-3 Business days");
+
+        onView(withId(R.id.select_transfer_method_types_list)).check(
+                matches(atPosition(2, hasDescendant(withText(wireTransferFee+wireTransferProcessingTime)))));
+
+        String paypalTransferFee = InstrumentationRegistry.getInstrumentation().getTargetContext()
+                .getString(R.string.noFee);
+        String paypalTransferProcessingTime = InstrumentationRegistry.getInstrumentation().getTargetContext()
+                .getString(R.string.processingTimeInformation, "IMMEDIATE");
+
+        onView(withId(R.id.select_transfer_method_types_list)).check(
+                matches(atPosition(3, hasDescendant(withText(paypalTransferFee+paypalTransferProcessingTime)))));
+
+
+    }
+
+    @Test
+    public void testSelectTransferMethod_verifyMixedFeeIsDisplayed() {
+        mMockWebServer.mockResponse().withHttpResponseCode(HTTP_OK).withBody(sResourceManager
+                .getResourceContent("user_response.json")).mock();
+        mMockWebServer.mockResponse().withHttpResponseCode(HTTP_OK).withBody(sResourceManager
+                .getResourceContent("successful_tmc_keys_mixedfee_response.json")).mock();
+
+        mActivityTestRule.launchActivity(null);
+
+        onView(withId(R.id.toolbar)).check(matches(isDisplayed()));
+        onView(withText(R.string.mobileAddTransferMethodHeader)).check(matches(withParent(withId(R.id.toolbar))));
+
+        onView(withId(R.id.select_transfer_method_country_value)).check(matches(withText("United States")));
+        onView(withId(R.id.select_transfer_method_currency_value)).check(matches(withText("USD")));
+        onView(withId(R.id.select_transfer_method_types_list)).check(new RecyclerViewCountAssertion(4));
+
+        onView(withId(R.id.select_transfer_method_country_value)).perform(click());
+        onView(allOf(withId(R.id.country_name), withText("CHINA"))).perform(click());
+
+        String mixedFee = InstrumentationRegistry.getInstrumentation().getTargetContext()
+                .getString(R.string.fee_mix_formatter,usdDollarSymbol,"5.00","4.50","4.00","10.00");
+
+        String venmoTransferFee = InstrumentationRegistry.getInstrumentation().getTargetContext()
+                .getString(R.string.feeInformation,mixedFee);
+
+        String venmoTransferProcessingTime = InstrumentationRegistry.getInstrumentation().getTargetContext()
+                .getString(R.string.processingTimeInformation, "1-2 Business days");
+
+        onView(withId(R.id.select_transfer_method_types_list)).check(
+                matches(atPosition(4, hasDescendant(withText(venmoTransferFee+venmoTransferProcessingTime)))));
+
+
     }
 }
--- transfermethodui/src/test/java/com/hyperwallet/android/ui/transfermethod/FeeFormatterTest.java
+++ transfermethodui/src/test/java/com/hyperwallet/android/ui/transfermethod/FeeFormatterTest.java
@@ -1,7 +1,9 @@
 package com.hyperwallet.android.ui.transfermethod;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.any;
 import static org.hamcrest.Matchers.is;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -9,6 +11,7 @@ import android.content.Context;
 import android.content.res.Resources;
 
 import com.hyperwallet.android.model.graphql.Fee;
+import com.hyperwallet.android.model.graphql.ProcessingTime;
 import com.hyperwallet.android.ui.R;
 import com.hyperwallet.android.ui.testutils.rule.HyperwalletExternalResourceManager;
 import com.hyperwallet.android.ui.transfermethod.view.FeeFormatter;
@@ -27,6 +30,7 @@ import org.mockito.junit.MockitoRule;
 import org.robolectric.RobolectricTestRunner;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 @RunWith(RobolectricTestRunner.class)
@@ -46,6 +50,8 @@ public class FeeFormatterTest {
     private Resources resources;
     private Fee mFlatFee;
     private JSONObject mJSONObject;
+    @Mock
+    private FeeFormatter mFeeFormatter;
 
     @Before
     public void setUp() throws JSONException {
@@ -147,5 +153,151 @@ public class FeeFormatterTest {
         List<Object> argumentList = formatterArgumentCapture.getAllValues();
         assertThat(resourceIdCaptorValue, is(R.string.fee_mix_only_min_formatter));
         assertThat(argumentList.size(), is(4));
+    }
+
+    @Test
+    public void testGetFormattedFee_returnsFlatNoFee() {
+        Fee flatFee = new Fee(mJSONObject.optJSONObject("FEE_NINE").optJSONArray("nodes").optJSONObject(0));
+        FeeFormatter.getFormattedFee(context, Arrays.asList(flatFee));
+        verify(resources).getString(resourceIdCaptor.capture());
+        int resourceIdCaptorValue = resourceIdCaptor.getValue();
+        assertThat(resourceIdCaptorValue, is(R.string.noFee));
+    }
+
+    @Test
+    public void testGetFormattedFee_returnsPercentNoFee() {
+        Fee percentFee = new Fee(mJSONObject.optJSONObject("FEE_EIGHT").optJSONArray("nodes").optJSONObject(0));
+        FeeFormatter.getFormattedFee(context, Arrays.asList(percentFee));
+        verify(resources).getString(resourceIdCaptor.capture());
+        int resourceIdCaptorValue = resourceIdCaptor.getValue();
+        assertThat(resourceIdCaptorValue, is(R.string.noFee));
+    }
+
+    @Test
+    public void testGetFormattedFee_returnsMixedNoFee() {
+        Fee flatFee = new Fee(mJSONObject.optJSONObject("FEE_EIGHT").optJSONArray("nodes").optJSONObject(0));
+        Fee percentFee = new Fee(mJSONObject.optJSONObject("FEE_NINE").optJSONArray("nodes").optJSONObject(0));
+
+        FeeFormatter.getFormattedFee(context, Arrays.asList(flatFee,percentFee));
+        verify(resources).getString(resourceIdCaptor.capture());
+        int resourceIdCaptorValue = resourceIdCaptor.getValue();
+        assertThat(resourceIdCaptorValue, is(R.string.noFee));
+    }
+
+    @Test
+    public void testGetFormattedFee_returnsFlatFee() {
+        Fee percentFee = new Fee(mJSONObject.optJSONObject("FEE_EIGHT").optJSONArray("nodes").optJSONObject(0));
+        FeeFormatter.getFormattedFee(context, Arrays.asList(mFlatFee,percentFee));
+
+        verify(resources).getString(resourceIdCaptor.capture(), formatterArgumentCapture.capture());
+        int resourceIdCaptorValue = resourceIdCaptor.getValue();
+        List<Object> argumentList = formatterArgumentCapture.getAllValues();
+        assertThat(resourceIdCaptorValue, is(R.string.fee_flat_formatter));
+        assertThat(argumentList.size(), is(2));
+    }
+
+    @Test
+    public void testGetFormattedFee_returnsFlatFeeWithMinAndMax() {
+        Fee percentFee = new Fee(mJSONObject.optJSONObject("FEE_TEN").optJSONArray("nodes").optJSONObject(0));
+        FeeFormatter.getFormattedFee(context, Arrays.asList(mFlatFee,percentFee));
+
+        verify(resources).getString(resourceIdCaptor.capture(), formatterArgumentCapture.capture());
+        int resourceIdCaptorValue = resourceIdCaptor.getValue();
+        List<Object> argumentList = formatterArgumentCapture.getAllValues();
+        assertThat(resourceIdCaptorValue, is(R.string.fee_flat_formatter));
+        assertThat(argumentList.size(), is(2));
+    }
+
+    @Test
+    public void testGetFormattedFee_returnsFlatFeeWithMaxOnly() {
+        Fee percentFee = new Fee(mJSONObject.optJSONObject("FEE_TWELVE").optJSONArray("nodes").optJSONObject(0));
+        FeeFormatter.getFormattedFee(context, Arrays.asList(mFlatFee,percentFee));
+
+        verify(resources).getString(resourceIdCaptor.capture(), formatterArgumentCapture.capture());
+        int resourceIdCaptorValue = resourceIdCaptor.getValue();
+        List<Object> argumentList = formatterArgumentCapture.getAllValues();
+        assertThat(resourceIdCaptorValue, is(R.string.fee_flat_formatter));
+        assertThat(argumentList.size(), is(2));
+    }
+
+    @Test
+    public void testGetFormattedFee_returnsFlatFeeWithMinOnly() {
+        Fee percentFee = new Fee(mJSONObject.optJSONObject("FEE_ELEVEN").optJSONArray("nodes").optJSONObject(0));
+        FeeFormatter.getFormattedFee(context, Arrays.asList(mFlatFee,percentFee));
+
+        verify(resources).getString(resourceIdCaptor.capture(), formatterArgumentCapture.capture());
+        int resourceIdCaptorValue = resourceIdCaptor.getValue();
+        List<Object> argumentList = formatterArgumentCapture.getAllValues();
+        assertThat(resourceIdCaptorValue, is(R.string.fee_flat_formatter));
+        assertThat(argumentList.size(), is(2));
+    }
+
+    @Test
+    public void testGetFormattedFee_returnsPercentFeeWithMinAndMax() {
+        Fee flatFee = new Fee(mJSONObject.optJSONObject("FEE_NINE").optJSONArray("nodes").optJSONObject(0));
+        Fee percentFee = new Fee(mJSONObject.optJSONObject("FEE_TWO").optJSONArray("nodes").optJSONObject(0));
+        FeeFormatter.getFormattedFee(context, Arrays.asList(flatFee,percentFee));
+
+        verify(resources).getString(resourceIdCaptor.capture(), formatterArgumentCapture.capture());
+        int resourceIdCaptorValue = resourceIdCaptor.getValue();
+        List<Object> argumentList = formatterArgumentCapture.getAllValues();
+        assertThat(resourceIdCaptorValue, is(R.string.fee_percent_formatter));
+        assertThat(argumentList.size(), is(4));
+    }
+
+    @Test
+    public void testGetFormattedFee_returnsPercentFeeWithMinOnly() {
+        Fee flatFee = new Fee(mJSONObject.optJSONObject("FEE_NINE").optJSONArray("nodes").optJSONObject(0));
+        Fee percentFee = new Fee(mJSONObject.optJSONObject("FEE_THREE").optJSONArray("nodes").optJSONObject(0));
+        FeeFormatter.getFormattedFee(context, Arrays.asList(flatFee,percentFee));
+
+        verify(resources).getString(resourceIdCaptor.capture(), formatterArgumentCapture.capture());
+        int resourceIdCaptorValue = resourceIdCaptor.getValue();
+        List<Object> argumentList = formatterArgumentCapture.getAllValues();
+        assertThat(resourceIdCaptorValue, is(R.string.fee_percent_only_min_formatter));
+        assertThat(argumentList.size(), is(3));
+    }
+
+    @Test
+    public void testGetFormattedFee_returnsPercentFeeWithMaxOnly() {
+        Fee flatFee = new Fee(mJSONObject.optJSONObject("FEE_NINE").optJSONArray("nodes").optJSONObject(0));
+        Fee percentFee = new Fee(mJSONObject.optJSONObject("FEE_FOUR").optJSONArray("nodes").optJSONObject(0));
+        FeeFormatter.getFormattedFee(context, Arrays.asList(flatFee,percentFee));
+
+        verify(resources).getString(resourceIdCaptor.capture(), formatterArgumentCapture.capture());
+        int resourceIdCaptorValue = resourceIdCaptor.getValue();
+        List<Object> argumentList = formatterArgumentCapture.getAllValues();
+        assertThat(resourceIdCaptorValue, is(R.string.fee_percent_only_max_formatter));
+        assertThat(argumentList.size(), is(3));
+    }
+
+    @Test
+    public void testGetFormattedFeeAndProcessingTime_returnsFormattedFeeAndProcessingTimeWithNoFeeString() throws JSONException {
+
+        JSONObject  mJSONObject = new JSONObject(externalResourceManager.getResourceContent("processing_time_information.json"));
+        ProcessingTime mProcessingTime = new ProcessingTime(mJSONObject.getJSONObject("PROCESSING_TIME_ONE").getJSONArray("nodes").optJSONObject(0));
+
+        when(FeeFormatter.getFormattedFee(context,Arrays.asList(mFlatFee))).thenReturn("No fee");
+        when(context.getResources().getString(R.string.noFee)).thenReturn("No fee");
+        when(context.getResources().getString(R.string.fee_flat_formatter)).thenReturn("$2.00");
+        when(context.getResources().getString(R.string.processingTimeInformation,"1-2 Business days")).thenReturn(" \u2022 1-2 Business days");
+
+        String formattedString = FeeFormatter.getFormattedFeeAndProcessingTime(context,Arrays.asList(mFlatFee),mProcessingTime);
+        assertThat(formattedString , is("No fee \u2022 1-2 Business days"));
+    }
+
+    @Test
+    public void testGetFormattedFeeAndProcessingTime_returnsFormattedFeeAndProcessingTimeWithFlatFeeString() throws JSONException {
+
+        JSONObject  mJSONObject = new JSONObject(externalResourceManager.getResourceContent("processing_time_information.json"));
+        ProcessingTime mProcessingTime = new ProcessingTime(mJSONObject.getJSONObject("PROCESSING_TIME_ONE").getJSONArray("nodes").optJSONObject(0));
+
+        when(FeeFormatter.getFormattedFee(context,Arrays.asList(mFlatFee))).thenReturn("$3.00 fee");
+        when(context.getResources().getString(R.string.noFee)).thenReturn("No fee");
+        when(context.getResources().getString(R.string.feeInformation,"$3.00 fee")).thenReturn("$3.00 fee");
+        when(context.getResources().getString(R.string.processingTimeInformation,"1-2 Business days")).thenReturn(" \u2022 1-2 Business days");
+
+        String formattedString = FeeFormatter.getFormattedFeeAndProcessingTime(context,Arrays.asList(mFlatFee),mProcessingTime);
+        assertThat(formattedString , is("$3.00 fee \u2022 1-2 Business days"));
     }
 }
